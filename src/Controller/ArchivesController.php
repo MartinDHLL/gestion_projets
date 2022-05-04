7abebea6aa84091ba588c92264cc9c771dd09798
step19 <martin.dhollande@outlook.fr>
2022-05-04 09:06:04 +0200

    ajout fichiers

---
+++ src/Controller/ArchivesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArchivesController extends AbstractController
+{
+    #[Route('/archives', name: 'app_archives')]
+    public function index(): Response
+    {
+        return $this->render('archives/index.html.twig', [
+            'controller_name' => 'ArchivesController',
+        ]);
+    }
+}
---
+++ src/Entity/Archive.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArchiveRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArchiveRepository::class)]
+class Archive
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $libelle;
+
+    #[ORM\Column(type: 'date')]
+    private $datedebut;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $datefin;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $budget;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $couts;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getDatedebut(): ?\DateTimeInterface
+    {
+        return $this->datedebut;
+    }
+
+    public function setDatedebut(\DateTimeInterface $datedebut): self
+    {
+        $this->datedebut = $datedebut;
+
+        return $this;
+    }
+
+    public function getDatefin(): ?\DateTimeInterface
+    {
+        return $this->datefin;
+    }
+
+    public function setDatefin(?\DateTimeInterface $datefin): self
+    {
+        $this->datefin = $datefin;
+
+        return $this;
+    }
+
+    public function getBudget(): ?int
+    {
+        return $this->budget;
+    }
+
+    public function setBudget(?int $budget): self
+    {
+        $this->budget = $budget;
+
+        return $this;
+    }
+
+    public function getCouts(): ?int
+    {
+        return $this->couts;
+    }
+
+    public function setCouts(?int $couts): self
+    {
+        $this->couts = $couts;
+
+        return $this;
+    }
+}
