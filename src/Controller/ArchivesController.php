ac9ceadbafebe75c9c1f5cd119d67ba11089aa05
step19 <martin.dhollande@outlook.fr>
2022-05-04 12:27:19 +0200

    dernières modifications

--- src/Controller/ArchivesController.php
+++ src/Controller/ArchivesController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Archive;
+use App\Entity\Projet;
+use App\Repository\ArchiveRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArchivesController extends AbstractController
 {
     #[Route('/archives', name: 'app_archives')]
-    public function index(): Response
+    public function index(ArchiveRepository $archiveRepository): Response
     {
+        $archives = $archiveRepository->findAll();
+
         return $this->render('archives/index.html.twig', [
-            'controller_name' => 'ArchivesController',
+            'archives' => $archives
         ]);
+    }
+
+    #[Route('/archives/make', name: 'app_makearchive')]
+    public function makeArchive(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $projetid = $request->get('projetid');
+        $em = $managerRegistry->getManager();
+        $projet = $em->getRepository(Projet::class)->find($projetid);
+
+        $archive = new Archive;
+
+        $archive->setLibelle($projet->getLibelle())
+        ->setDatedebut($projet->getDateDebut())
+        ->setDatefin($projet->getDateFin())
+        ->setBudget($projet->getBudget())
+        ->setCouts($projet->getCouts());
+
+        $em->persist($archive);
+        $em->flush();
+
+        $em->remove($projet);
+        
+        return $this->redirectToRoute('app_projets');
     }
 }
