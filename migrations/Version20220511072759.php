24e19ab679939877a737798f2b79648604c5fdfc
step19 <martin.dhollande@outlook.fr>
2022-05-11 09:28:25 +0200

    correction bugs migrations

--- migrations/Version20220511072759.php
+++ migrations/Version20220511072759.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220510122524 extends AbstractMigration
+final class Version20220511072759 extends AbstractMigration
 {
     public function getDescription(): string
     {
